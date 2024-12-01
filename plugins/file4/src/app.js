6abc099d3b6e9d9a5483d1644b950b870fa53d07
developer <ukohae39@gmail>
2024-11-30 23:10:10 -0500

    bump file4 to v1.0.3

--- plugins/file4/src/app.js
+++ plugins/file4/src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from file4 app!');
+  res.send('Hello from file4 app!!!');
 });
 
 app.listen(3004, () => {
